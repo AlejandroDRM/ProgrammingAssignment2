1957da56000b262d30e45a55988d8415f0ae02c9
melechever <melechever@gmail.com>
2014-04-23 22:30:41 -0430

    Final Solution
    This is my final solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
+## Assignment 2
+## Alejandro Rivero
 ## Put comments here that give an overall description of what your
 ## functions do
 
 ## Write a short comment describing this function
-
+## Construct special matrix to compute inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse <- NULL
+  set <- function(y) {
+    x <<- y
+    inverse<<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) inverse <<- solve
+  getsolve <- function() inverse
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
 ## Write a short comment describing this function
+## Compute the inverse of a swaure matrix using the data saved
+## in cache if available.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Return a matrix that is the inverse of 'x'
+  inverse <- x$getsolve() 
+  if(!is.null(inverse)) {
+    message("getting cached data")
+    return(inverse)
+  }
+  data <- x$get()
+  ##Compute elapsed time
+  startTime <- Sys.time()
+  inverse <- solve(data, ...)
+  endTime <- Sys.time()
+  time <- endTime - startTime
+  print(time)
+  x$setsolve(inverse)
+  inverse
 }
